072f25d9d29c820b09afe3ec20ad02f34a23141f
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-22 19:48:41 -0700

    Commit to migrate

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const CONFIGURATION = {
+	"development": {
+    	"dialect": "postgres",
+    	"username": "postgres",
+    	"password": process.env.LOCAL_DB_PW,
+    	"host": "localhost",
+    	"port": 1234,
+    	"database": "postgres"
+    },
+  	"production": {
+    	"dialect": "postgres",
+    	"username": process.env.HEROKU_DB_USER,
+    	"password": process.env.HEROKU_DB_PW,
+    	"host": process.env.HEROKU_DB_HOST,
+    	"port": 5432,
+    	"database": process.env.HEROKU_DB_NAME
+  	}
+}
+
+const env = process.env.NODE_ENV || "development";
+
+module.exports = CONFIGURATION[env]
