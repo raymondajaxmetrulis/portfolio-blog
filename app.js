023d51e182c1ebeb7ca44c49df32c124f42cd497
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-22 20:48:12 -0700

    another attempt to deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+var favicon = require('serve-favicon');
 require('dotenv').config();
 
 const app = express();
