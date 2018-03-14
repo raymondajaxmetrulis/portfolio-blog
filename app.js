71dd9934c80177a72ff0cdf7c6d39c2af6d8d08f
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-03-13 19:52:41 -0600

    More heroku troubleshooting. Remove favicon, google original error

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-var favicon = require('serve-favicon');
 
 require('dotenv').config();
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-var app = require('app');
+var app = require('./app');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.CONN_STRING || 'postgres://postgres@localhost:1234/postgres');
 
