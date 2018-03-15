55c204b0ff02baebc793fbfdc39d411e4a399259
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-03-14 20:02:12 -0600

    Remember to remove .env from .gitignore for heroku deployment

--- app.js
+++ app.js
@@ -25,44 +25,6 @@ app.use('/portfolio', portfolioRoute);
 app.use('/blog', blogRoute);
 app.use('/swoop', postRoute);
 
-app.use('*', function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-})
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found Okay');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'production') {
-  app.use(function(err, req, res, next) {
-    console.error('PROD ERROR')
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-} else {
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    console.error('DEV ERROR')
-    res.json({
-      message: err.message,
-      error: {}
-    });
-  });
-}
-
 const port = process.env.PORT || 3000;
 app.listen(port);
 
