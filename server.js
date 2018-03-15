e8bdfa8c3fc4cc4947b9dc0f150f90e90d152183
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-03-14 18:47:30 -0600

    Explicit connection string in server.js to fix sequelize error

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 var app = require('./app');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(process.env.CONN_STRING);
+var sequelize = new Sequelize('postgres://nyjfrtzqiyosgx:0fd4ec500fa4747cd8bc1ba4bec588bcec50ab8b4c29887aeb2e8f9de7380905@ec2-107-21-236-219.compute-1.amazonaws.com:5432/dcvkelnn8bu7qc');
 
 module.exports = app;
